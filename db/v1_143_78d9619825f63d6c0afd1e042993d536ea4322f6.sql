4fa2d886e7f2f8187884e152d9b129d91c4ac336
laocui <cuihailiang9@gmail.com>
2012-11-28 11:22:46 +0800

    修改logo存放地址及命名方式

--- db/v1_143_78d9619825f63d6c0afd1e042993d536ea4322f6.sql
+++ db/v1_143_78d9619825f63d6c0afd1e042993d536ea4322f6.sql
@@ -4,7 +4,7 @@ delete from SYS_INFO_ITEM_REF t where t.item_id like 'C%'
 
 --配置公司logo
 执行以下语句：insert into SYS_PARAMETER (PARA_KEY, PARA_VALUE, PARA_GROUP, PARA_VALUE_CN, PARA_VALUE_TW, PARA_VALUE_EN)
-values ('SYS_INTERFACE', 'images/maininterface/login_backgroud_sias.jpg,images/maininterface/logo_sias.png', null, null, null, null);
+values ('SYS_INTERFACE', 'images/logo/kingstar_login_backgroud.jpg,images/logo/kingstar_logo.png', null, null, null, null);
 说明:PARA_VALUE值前一个为登陆页背景图片后一个为公司logo中间用逗号隔开
 
 --添加人员报错问题
