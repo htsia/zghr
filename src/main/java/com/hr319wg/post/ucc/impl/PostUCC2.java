e43260e5d31cc76ac692476b8d72d214e8404571
laocui <cuihailiang9@gmail.com>
2013-01-07 10:47:42 +0800

    修改岗位维护 oracel 11.2 下的SQL错误

--- src/main/java/com/hr319wg/post/ucc/impl/PostUCC2.java
+++ src/main/java/com/hr319wg/post/ucc/impl/PostUCC2.java
@@ -1,37 +1,18 @@
 package com.hr319wg.post.ucc.impl;
 
+import java.util.Hashtable;
+
 import com.hr319wg.common.Constants;
-import com.hr319wg.common.exception.RollbackableException;
 import com.hr319wg.common.exception.SysException;
 import com.hr319wg.common.pojo.vo.User;
-import com.hr319wg.org.pojo.bo.OrgBO;
-import com.hr319wg.post.pojo.bo.PostBO;
-import com.hr319wg.post.pojo.vo.PostVO;
-import com.hr319wg.post.service.PostService;
-import com.hr319wg.post.ucc.IPostUCC;
-import com.hr319wg.post.util.PostTool;
-import com.hr319wg.sys.api.QueryAPI;
-import com.hr319wg.sys.api.SysAPI;
-import com.hr319wg.sys.cache.SysCache;
-import com.hr319wg.sys.cache.SysCacheTool;
-import com.hr319wg.sys.pojo.bo.InfoSetBO;
 import com.hr319wg.sys.pojo.vo.CellVO;
-import com.hr319wg.sys.pojo.vo.RecordVO;
 import com.hr319wg.sys.pojo.vo.TableVO;
-import com.hr319wg.sys.service.ActivePageService;
 import com.hr319wg.util.CommonFuns;
-import com.hr319wg.util.SequenceGenerator;
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.List;
 
 public class PostUCC2
 extends PostUCC
 {
-	 private PostService postservice;
-	  private ActivePageService activepageservice;
-	  private QueryAPI queryapi;
-	  private SysAPI sysapi;
+
 	public String queryPostList(TableVO table, String postName, String superId, int pageNum, int rowNum, String cancel, User user, String qid) throws SysException {
 		StringBuffer where = new StringBuffer();
 		if ((postName != null) && (!("".equals(postName)))) {
@@ -50,10 +31,10 @@ extends PostUCC
 			where.append(" and C001.C001730 = '").append(cancel).append("'");
 		}
 
-		CellVO[] c1 = this.queryapi.queryInfoItem(qid);
+		CellVO[] c1 = this.getQueryapi().queryInfoItem(qid);
 		table.setHeader(c1);
 		table.setSetType("C");
-		Hashtable hash = this.queryapi.getQuerySqlHash(user, qid);
+		Hashtable hash = this.getQueryapi().getQuerySqlHash(user, qid);
 		String select = CommonFuns.filterNull((String)hash.get("SQL_SELECT_PART"));
 		String from = CommonFuns.filterNull((String)hash.get("SQL_FROM_PART"));
 		String condition = CommonFuns.filterNull((String)hash.get("SQL_WHERE_PART"));
@@ -81,33 +62,11 @@ extends PostUCC
 
 		}
 
-		this.activepageservice.querySql(table, sql.toString(), user, pageNum, rowNum, true);
+		this.getActivepageservice().querySql(table, sql.toString(), user, pageNum, rowNum, true);
 		return sql.toString();
 	}
-	public PostService getPostservice() {
-		return postservice;
-	}
-	public void setPostservice(PostService postservice) {
-		this.postservice = postservice;
-	}
-	public ActivePageService getActivepageservice() {
-		return activepageservice;
-	}
-	public void setActivepageservice(ActivePageService activepageservice) {
-		this.activepageservice = activepageservice;
-	}
-	public QueryAPI getQueryapi() {
-		return queryapi;
-	}
-	public void setQueryapi(QueryAPI queryapi) {
-		this.queryapi = queryapi;
-	}
-	public SysAPI getSysapi() {
-		return sysapi;
-	}
-	public void setSysapi(SysAPI sysapi) {
-		this.sysapi = sysapi;
-	}
+
+
 
 	
 }
