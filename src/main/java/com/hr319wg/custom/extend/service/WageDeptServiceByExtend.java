de421b9b66c4c05101bf48b3ec0dc4d6c7b9fa4e
haowei <594973849@qq.com>
2013-02-04 11:12:49 +0800

    修正薪资部门人员同步

---
+++ src/main/java/com/hr319wg/custom/extend/service/WageDeptServiceByExtend.java
@@ -0,0 +1,16 @@
+package com.hr319wg.custom.extend.service;
+
+import com.hr319wg.common.exception.SysException;
+import com.hr319wg.wage.pojo.bo.WageDeptBO;
+import com.hr319wg.wage.service.WageDeptService;
+
+public class WageDeptServiceByExtend extends WageDeptService {
+
+	public void updateData(String itemID) throws SysException {
+		WageDeptBO deptbo = (WageDeptBO) this.getWagedeptdao().findBoById(WageDeptBO.class, itemID);
+		if ((deptbo != null) && (deptbo.getLinkOrgs() != null) && (!"".equals(deptbo.getLinkOrgs())) && (deptbo.getName() != null)) {
+			String sql = "update a001 set A001743='"+ deptbo.getName()+ "' where a001705 in (select b.orguid from b001 b,b001 b2 where b.b001003 like b2.b001003 || '%' and b2.orguid in ("+deptbo.getLinkOrgs()+"))";
+			this.getWageapi().executeSql(sql);
+		}
+	}
+}
