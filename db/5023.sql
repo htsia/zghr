1b5656e01c4022b430ba6c75e33dace51ebec696
haowei <594973849@qq.com>
2013-05-16 03:35:31 +0800

    增加增减情况子集字段(分别原和现单位、部门、岗位)

---
+++ db/5023.sql
@@ -0,0 +1,24 @@
+
+insert into SYS_DB_VERSION values('5023',sysdate);
+
+
+--添加增减情况子集字段
+delete from sys_info_item s where s.item_id in ('A016200','A016201','A016202','A016203','A016204','A016205');
+
+insert into sys_info_item (ITEM_ID, SET_ID, ITEM_NAME, ITEM_DESC, ITEM_TYPE, ITEM_LENGTH, ITEM_PRECISION, ITEM_MAX, ITEM_MIN, CODE_SET_ID, ITEM_NOTNULL, ITEM_SEQ, ITEM_STATUS, ITEM_PROPERTY, ITEM_DEFAULTVALUE, ITEM_FORMULA, ITEM_ISSUE, IS_SELF_SHOW, SELECT_TYPE, ITEM_NAME_CN, ITEM_NAME_TW, ITEM_NAME_EN, ITEM_DESC_CN, ITEM_DESC_TW, ITEM_DESC_EN, IS_UNIQUE, CHARSET, ITEM_CANINPORT, ITEM_TRIGGER, ITEM_INWAGE, ITEM_MUST, CODE_BY_ORG, JY1006BH, JY1006NAME, GB14946, ITEM_CLASS)
+values ('A016200', 'A016', '原单位', null, '7', '50', null, null, null, 'OU', '0', '50', '0', '2', null, null, null, '1', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+
+insert into sys_info_item (ITEM_ID, SET_ID, ITEM_NAME, ITEM_DESC, ITEM_TYPE, ITEM_LENGTH, ITEM_PRECISION, ITEM_MAX, ITEM_MIN, CODE_SET_ID, ITEM_NOTNULL, ITEM_SEQ, ITEM_STATUS, ITEM_PROPERTY, ITEM_DEFAULTVALUE, ITEM_FORMULA, ITEM_ISSUE, IS_SELF_SHOW, SELECT_TYPE, ITEM_NAME_CN, ITEM_NAME_TW, ITEM_NAME_EN, ITEM_DESC_CN, ITEM_DESC_TW, ITEM_DESC_EN, IS_UNIQUE, CHARSET, ITEM_CANINPORT, ITEM_TRIGGER, ITEM_INWAGE, ITEM_MUST, CODE_BY_ORG, JY1006BH, JY1006NAME, GB14946, ITEM_CLASS)
+values ('A016201', 'A016', '新单位', null, '7', '50', null, null, null, 'OU', '0', '51', '0', '2', null, null, null, '1', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+
+insert into sys_info_item (ITEM_ID, SET_ID, ITEM_NAME, ITEM_DESC, ITEM_TYPE, ITEM_LENGTH, ITEM_PRECISION, ITEM_MAX, ITEM_MIN, CODE_SET_ID, ITEM_NOTNULL, ITEM_SEQ, ITEM_STATUS, ITEM_PROPERTY, ITEM_DEFAULTVALUE, ITEM_FORMULA, ITEM_ISSUE, IS_SELF_SHOW, SELECT_TYPE, ITEM_NAME_CN, ITEM_NAME_TW, ITEM_NAME_EN, ITEM_DESC_CN, ITEM_DESC_TW, ITEM_DESC_EN, IS_UNIQUE, CHARSET, ITEM_CANINPORT, ITEM_TRIGGER, ITEM_INWAGE, ITEM_MUST, CODE_BY_ORG, JY1006BH, JY1006NAME, GB14946, ITEM_CLASS)
+values ('A016202', 'A016', '原部门', null, '7', '50', null, null, null, 'OU', '0', '52', '0', '2', null, null, null, '1', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+
+insert into sys_info_item (ITEM_ID, SET_ID, ITEM_NAME, ITEM_DESC, ITEM_TYPE, ITEM_LENGTH, ITEM_PRECISION, ITEM_MAX, ITEM_MIN, CODE_SET_ID, ITEM_NOTNULL, ITEM_SEQ, ITEM_STATUS, ITEM_PROPERTY, ITEM_DEFAULTVALUE, ITEM_FORMULA, ITEM_ISSUE, IS_SELF_SHOW, SELECT_TYPE, ITEM_NAME_CN, ITEM_NAME_TW, ITEM_NAME_EN, ITEM_DESC_CN, ITEM_DESC_TW, ITEM_DESC_EN, IS_UNIQUE, CHARSET, ITEM_CANINPORT, ITEM_TRIGGER, ITEM_INWAGE, ITEM_MUST, CODE_BY_ORG, JY1006BH, JY1006NAME, GB14946, ITEM_CLASS)
+values ('A016203', 'A016', '新部门', null, '7', '20', null, null, null, 'OU', '0', '53', '0', '2', null, null, null, '1', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+
+insert into sys_info_item (ITEM_ID, SET_ID, ITEM_NAME, ITEM_DESC, ITEM_TYPE, ITEM_LENGTH, ITEM_PRECISION, ITEM_MAX, ITEM_MIN, CODE_SET_ID, ITEM_NOTNULL, ITEM_SEQ, ITEM_STATUS, ITEM_PROPERTY, ITEM_DEFAULTVALUE, ITEM_FORMULA, ITEM_ISSUE, IS_SELF_SHOW, SELECT_TYPE, ITEM_NAME_CN, ITEM_NAME_TW, ITEM_NAME_EN, ITEM_DESC_CN, ITEM_DESC_TW, ITEM_DESC_EN, IS_UNIQUE, CHARSET, ITEM_CANINPORT, ITEM_TRIGGER, ITEM_INWAGE, ITEM_MUST, CODE_BY_ORG, JY1006BH, JY1006NAME, GB14946, ITEM_CLASS)
+values ('A016204', 'A016', '原岗位', null, '11', '50', null, null, null, 'PO', '0', '54', '0', '2', null, null, null, '1', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+
+insert into sys_info_item (ITEM_ID, SET_ID, ITEM_NAME, ITEM_DESC, ITEM_TYPE, ITEM_LENGTH, ITEM_PRECISION, ITEM_MAX, ITEM_MIN, CODE_SET_ID, ITEM_NOTNULL, ITEM_SEQ, ITEM_STATUS, ITEM_PROPERTY, ITEM_DEFAULTVALUE, ITEM_FORMULA, ITEM_ISSUE, IS_SELF_SHOW, SELECT_TYPE, ITEM_NAME_CN, ITEM_NAME_TW, ITEM_NAME_EN, ITEM_DESC_CN, ITEM_DESC_TW, ITEM_DESC_EN, IS_UNIQUE, CHARSET, ITEM_CANINPORT, ITEM_TRIGGER, ITEM_INWAGE, ITEM_MUST, CODE_BY_ORG, JY1006BH, JY1006NAME, GB14946, ITEM_CLASS)
+values ('A016205', 'A016', '新岗位', null, '11', '50', null, null, null, 'PO', '0', '55', '0', '2', null, null, null, '1', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
--- src/main/java/com/hr319wg/custom/extend/ucc/PersonUCCByExtend.java
+++ src/main/java/com/hr319wg/custom/extend/ucc/PersonUCCByExtend.java
@@ -4,15 +4,25 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 import com.hr319wg.common.Constants;
+import com.hr319wg.common.exception.RollbackableException;
 import com.hr319wg.common.exception.SysException;
 import com.hr319wg.common.pojo.vo.User;
+import com.hr319wg.common.web.SysContext;
+import com.hr319wg.emp.pojo.bo.ChangeInfoBO;
+import com.hr319wg.emp.pojo.bo.PersonBO;
+import com.hr319wg.emp.pojo.vo.PersonChangeVO;
 import com.hr319wg.emp.ucc.impl.PersonUCC;
 import com.hr319wg.org.pojo.bo.OrgBO;
+import com.hr319wg.org.util.OrgTool;
+import com.hr319wg.portal.IUUManager;
+import com.hr319wg.sys.cache.SysCache;
 import com.hr319wg.sys.cache.SysCacheTool;
 import com.hr319wg.sys.pojo.bo.InfoItemBO;
 import com.hr319wg.sys.pojo.vo.CellVO;
 import com.hr319wg.sys.pojo.vo.TableVO;
+import com.hr319wg.util.CodeUtil;
 import com.hr319wg.util.CommonFuns;
+import com.hr319wg.util.SequenceGenerator;
 
 public class PersonUCCByExtend extends PersonUCC {
 
@@ -160,4 +170,87 @@ public class PersonUCCByExtend extends PersonUCC {
 	    sql = "delete from B730 where B730700='" + pid + "'";
 	    this.getActivepageservice().executeSql(sql);
 	  }
+	 
+	public void chgDismiss(PersonChangeVO personchangevo,
+			ChangeInfoBO changeinfo, String[] ids) throws RollbackableException {
+		try {
+			String depID = changeinfo.getToDepId();
+			String postID = CommonFuns.filterNull(changeinfo.getToPostId());
+			String postLevel = CommonFuns.filterNull(changeinfo.getToPostLevel());
+
+			OrgBO orgx = null;
+			String xt = "";
+			String orgTreeid = "";
+			String depTreeid = "";
+
+			if ((depID != null) && (!depID.equals(""))) {
+				OrgBO superOrg = OrgTool.getOrgByDept(depID);
+				if (superOrg != null) {
+					xt = superOrg.getOrgId();
+					orgTreeid = superOrg.getTreeId();
+					changeinfo.setToOrgId(xt);
+				}
+				orgx = SysCacheTool.findOrgById(depID);
+				if (orgx != null) {
+					depTreeid = orgx.getTreeId();
+
+					ArrayList list = new ArrayList();
+					for (int i = 0; i < ids.length; i++) {
+						PersonBO po = SysCacheTool.findPersonById(ids[i]);
+						if (po == null) {
+							continue;
+						}
+						//机构人员流动
+						String b730subid = SequenceGenerator.getKeyId("B730");
+			            list.add("insert into B730(orguid,subid,B730000,B730700,B730701,B730702,B730703) values('" + po.getOrgId() + "','" + CommonFuns.filterNull(b730subid) + "','" + "00900" + "','" + ids[i] + "','" + personchangevo.getChangeDate() + "','" + personchangevo.getChangeType() + "','" + superOrg.getName() + "')");
+			            //增减情况子集
+			            list.add("insert into A016(id,subid,A016010,A016020,A016030,A016040,A016045,A016050) values('" + ids[i] + "','" + SequenceGenerator.getKeyId("A016") + "','" + PersonChangeVO.CHANGE_TYPE_OUT + "','" + CommonFuns.filterNull(changeinfo.getApplyDate()) + "','" + CommonFuns.filterNull(CodeUtil.interpertCode(CodeUtil.TYPE_ORG, changeinfo.getFromOrgId())) + "','" + CommonFuns.filterNull(changeinfo.getApplyDate()) + "','" + CommonFuns.filterNull(changeinfo.getFromOperator()) + "','" + b730subid + "')");
+			            
+			            OrgBO or = SysCacheTool.findOrgById(po.getOrgId());
+			            b730subid = SequenceGenerator.getKeyId("B730");
+			            list.add("insert into B730(orguid,subid,B730000,B730700,B730701,B730702,B730703) values('" + xt + "','" + b730subid + "','" + "00900" + "','" + ids[i] + "','" + personchangevo.getChangeDate() + "','0200300008','" + or.getName() + "')");
+
+			            list.add("insert into A016(id,subid,A016010,A016020,A016030,A016040,A016045,A016050,A016200,A016201,A016202,A016203,A016204,A016205) values" +
+			            		"('" + ids[i] + "','" + SequenceGenerator.getKeyId("A016") + "','" + PersonChangeVO.CHANGE_TYPE_IN + "','" + CommonFuns.filterNull(personchangevo.getChangeDate()) + "'," +
+			            				"'" + CommonFuns.filterNull(superOrg.getName()) + "','" + CommonFuns.filterNull(personchangevo.getTractDate()) + "','" + CommonFuns.filterNull(personchangevo.getTractPerson()) + "','" + b730subid + "','"+po.getOrgId()+"','"+xt+"','"+po.getDeptId()+"','"+orgx.getOrgId()+"','"+po.getPostId()+"','"+postID+"')");
+			            //工作简历子集
+			            list.add("update A019 set A019010='" + CommonFuns.filterNull(personchangevo.getTractDate()) + "' where id='" + ids[i] + "' and A019000='00901'");
+			            list.add("update A019 set A019000='00900' where id='" + ids[i] + "'");
+			            list.add("insert into A019(id,subid,A019000,A019005,A019015,A019020,A019025,A019700) values('" + ids[i] + "','" + SequenceGenerator.getKeyId("A019") + "','00901','" + CommonFuns.filterNull(personchangevo.getTractDate()) + "','" + CommonFuns.filterNull(superOrg.getName()) + "','" + CommonFuns.filterNull(CodeUtil.interpertCode(CodeUtil.TYPE_POST, changeinfo.getToPostId())) + "','" + CommonFuns.filterNull(CodeUtil.interpertCode(CodeUtil.TYPE_PERSON, personchangevo.getTractPerson())) + "','内部调动')");
+					}
+
+					if ((list != null) && (list.size() > 0)) {
+						this.getActivepageservice().batchExecuteSql(
+								(String[]) (String[]) list
+										.toArray(new String[0]));
+					}
+
+					String sql = "update A001 set A001701 = '" + xt
+							+ "',A001705='" + depID + "',A001728='" + orgTreeid
+							+ "',A001738='" + depTreeid + "',A001715='"
+							+ postID + "',A001716='" + postLevel + "' where  "
+							+ CommonFuns.splitInSql(ids, "ID");
+					this.getActivepageservice().executeSql(sql);
+
+					if (!"".equals(Constants.UUM_SERVICE_NAME))
+						try {
+							IUUManager uumanager = (IUUManager) SysContext
+									.getBean(Constants.UUM_SERVICE_NAME);
+							if (uumanager != null)
+								for (int i = 0; i < ids.length; i++) {
+									String[] id = new String[1];
+									id[0] = ids[i];
+									SysCache.setMap(id, 2, 6);
+									uumanager.userModify(id[0], personchangevo.getTractPerson(), "1");
+								}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+				}
+			}
+		} catch (Exception e) {
+			throw new RollbackableException("E070001", "同步信息子集出错！"
+					+ e.getMessage(), e, getClass());
+		}
+	}
 }
