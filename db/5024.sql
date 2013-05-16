e2b011f50895b38456be2bec1b23ac2707da5a9d
haowei <594973849@qq.com>
2013-05-16 10:25:03 +0800

    人员异动导航图增加人员划转

---
+++ db/5024.sql
@@ -0,0 +1,7 @@
+
+insert into SYS_DB_VERSION values('5024',sysdate);
+
+
+--人员异动导航图增加人员划转
+insert into sys_navigate (NAVI_ID, MODULE_ID, OPERATE_ID, COORDS)
+values ('0329', 'RYDP', '0329', '435,270,505,335');
